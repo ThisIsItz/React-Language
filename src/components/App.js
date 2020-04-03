2b396c510d2a054acb582ef7e4ad3f82233f8f10
ThisIsItz <itziarurbieta94@gmail.com>
2020-04-03 09:43:52 +0100

    Added UI and select language

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,22 @@
 import React from 'react'
 
 class App extends React.Component{
+    state = { language: 'english' }
+
+    onLanguageChange = language => {
+        this.setState({ language: language })
+    }
+
     render(){
         return(
-            <div>App</div>
+            <div className="ui container">
+                <div>
+                    Select a language:
+                    <i className="flag uk" onClick={() => this.onLanguageChange('english')} />
+                    <i className="flag es" onClick={() => this.onLanguageChange('spanish')} />
+                </div>
+                {this.state.language}
+            </div>
         )
     }
 }
