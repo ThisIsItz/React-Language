cdadd19fe475977c905ee486d4fc3fcee0563d0a
ThisIsItz <itziarurbieta94@gmail.com>
2020-04-03 13:30:47 +0100

    Created context object

--- src/components/App.js
+++ src/components/App.js
@@ -11,11 +11,9 @@ class App extends React.Component{
     render(){
         return(
             <div className="ui container">
-                <div>
                     Select a language:
                     <i className="flag uk" onClick={() => this.onLanguageChange('english')} />
                     <i className="flag es" onClick={() => this.onLanguageChange('spanish')} />
-                </div>
                 <UserCreate/>
             </div>
         )
